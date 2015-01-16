a4074d52a325d1fea90971310b4bdb5da8318083
abhisheksamuelds <abhisheksamuelds@gmail.com>
2015-01-16 15:19:33 +0800

    Update har.R

--- har.R
+++ har.R
@@ -1,33 +1,32 @@
 library(kernlab)
 library(caret)
-library(corrplot)
 library(randomForest)
 
 data_training <- read.csv("D:/Learning/Statictics/Practical Machine Learning/Human Activity Recognition/input/pml-training.csv", na.strings= c("NA",""," "))
 data_testing <- read.csv("D:/Learning/Statictics/Practical Machine Learning/Human Activity Recognition/input/pml-testing.csv", na.strings= c("NA",""," "))
 
-data_training_NAs <- apply(data_training, 2, function(x) {sum(is.na(x))})
-data_training_clean <- data_training[,which(data_training_NAs == 0)]
+data_training_filter <- apply(data_training, 2, function(x) {sum(is.na(x))})
+data_training_clean <- data_training[,which(data_training_filter == 0)]
 data_training_clean <- data_training_clean[8:length(data_training_clean)]
 
-data_test_NAs <- apply(data_testing, 2, function(x) {sum(is.na(x))})
-data_test_clean <- data_testing[,which(data_test_NAs == 0)]
-data_test_clean <- data_test_clean[8:length(data_test_clean)]
+data_test_filter <- apply(data_testing, 2, function(x) {sum(is.na(x))})
+data_testing_clean <- data_testing[,which(data_test_filter == 0)]
+data_testing_clean <- data_testing_clean[8:length(data_testing_clean)]
 
 inTrain <- createDataPartition(y = data_training_clean$classe, p = 0.75, list = FALSE)
 training <- data_training_clean[inTrain, ]
-crossval <- data_training_clean[-inTrain, ]
+crossValidation <- data_training_clean[-inTrain, ]
 
 model <- randomForest(classe ~ ., data = training)
 
-predictTrain <- predict(model, training)
-confusionMatrix(training$classe, predictTrain)
+predictTraining <- predict(model, training)
+confusionMatrix(training$classe, predictTraining)
 
-predictCrossVal <- predict(model, crossval)
-confusionMatrix(crossval$classe, predictCrossVal)
+predictCrossValidation <- predict(model, crossValidation)
+confusionMatrix(crossValidation$classe, predictCrossValidation)
 
-predictTest <- predict(model, data_test_clean)
-predictTest
+predictTesting <- predict(model, data_testing_clean)
+predictTesting
 
 write.pml.predictions <- function(x) {
 n = length(x)
@@ -37,4 +36,4 @@ write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
 }
 }
 
-write.pml.predictions(predictTest)
+write.pml.predictions(predictTesting)
